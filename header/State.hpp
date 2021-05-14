93a611242ef85f5863b58841ce6508639c1f11e2
SrijanPagadala <srijanrpagadala@gmail.com>
2021-05-14 16:52:25 -0700

    Added skeleton for State.hpp

---
+++ header/State.hpp
@@ -0,0 +1,17 @@
+#ifndef __STATE_HPP__
+#define __STATE_HPP__
+
+#include <iostream>
+using namespace std;
+
+class State {
+    private:
+
+    public:
+        virtual void display() = 0;
+
+
+
+};
+
+#endif
