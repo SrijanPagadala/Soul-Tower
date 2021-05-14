3f2c6882fa3b22867dceac1604c33cd7c3b01b54
SrijanPagadala <srijanrpagadala@gmail.com>
2021-05-14 16:57:48 -0700

    Created skeleton for FightState.hpp

---
+++ header/FightState.hpp
@@ -0,0 +1,23 @@
+#ifndef __FIGHTSTATE_HPP__
+#define __FIGHTSTATE_HPP__
+
+#include <iostream>
+#include "State.hpp"
+using namespace std;
+
+
+class FightState: public State {
+    private:
+
+    public:
+    void display();
+
+
+
+
+};
+
+
+
+
+#endif
