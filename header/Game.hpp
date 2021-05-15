ae6af6157725fbb9d5ce2a1d1ab50bc3fef8d01d
SrijanPagadala <srijanrpagadala@gmail.com>
2021-05-14 17:07:43 -0700

    Added State pointer & default constructor to Game.hpp

--- header/Game.hpp
+++ header/Game.hpp
@@ -2,12 +2,15 @@
 #define __GAME_HPP__
 
 #include <iostream>
+#include "State.hpp"
 using namespace std;
 
 class Game {
     private:
         double coins;
+        State* currState;
     public:
+        Game() : coins(0), currState(nullptr) {}
         double getCoins() { return coins; }
         void setCoins(double newAmount) { coins = newAmount; }
         void changeCoins(double amount) { coins += amount; }
