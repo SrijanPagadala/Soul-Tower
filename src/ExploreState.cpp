8ce7b5b3c8f5d54e1d9bb05f6e04d123a3cd0b77
SrijanPagadala <srijanrpagadala@gmail.com>
2021-05-14 18:39:09 -0700

    Continued explore state

--- src/ExploreState.cpp
+++ src/ExploreState.cpp
@@ -12,7 +12,9 @@ void ExploreState::display(){
     if(level == 1){ //first level exploration
         cout << "You wake up at the bottom of a large tower, very confused on how you got there." << endl;
         cout << "You notice a large spiral staircase, stretching to the top of the tower." << endl;
-
+        cout << "At the base of the stairs, you see a sign that states \"SOUL TOWER: ENTER AT YOUR OWN RISK.\"" << endl;
+        cout << "Being the tremendously smart traveler you are, you decide to step up the stairs." << endl;
+        cout << "You hear a deep voice: \"WELCOME TO THE FIRST FLOOR\"" << endl;
 
     }
 
