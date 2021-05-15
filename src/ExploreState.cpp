b5355a97cb1a50572a9f76ec0c3975498faa1b97
SrijanPagadala <srijanrpagadala@gmail.com>
2021-05-14 17:38:09 -0700

    Added constructor implementations

--- src/ExploreState.cpp
+++ src/ExploreState.cpp
@@ -1,6 +1,14 @@
 #include "../header/ExploreState.hpp"
 
+ExploreState::ExploreState(){
+    level = 1; 
+}
+
+ExploreState::ExploreState(int levelToSet){
+    level = levelToSet;
+}
+
 void ExploreState::display(){
-    
+
 
 }
