2f70b0164f6ce461c95183eed307b20dfd852cc3
SrijanPagadala <srijanrpagadala@gmail.com>
2021-05-14 18:35:22 -0700

    Started on level 1 exploration dialogue

--- src/ExploreState.cpp
+++ src/ExploreState.cpp
@@ -9,6 +9,11 @@ ExploreState::ExploreState(int levelToSet){
 }
 
 void ExploreState::display(){
+    if(level == 1){ //first level exploration
+        cout << "You wake up at the bottom of a large tower, very confused on how you got there." << endl;
+        cout << "You notice a large spiral staircase, stretching to the top of the tower." << endl;
 
+
+    }
 
 }
