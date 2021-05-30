273af3eabdbc24b2438e4075fbd5f8d6bd5afa5a
nabil-k <duomaster01@gmail.com>
2021-05-30 16:47:03 -0700

    Made warrior use armor before being attacked

--- header/Warrior.hpp
+++ header/Warrior.hpp
@@ -12,19 +12,19 @@ public:
     Warrior(cMain* gui) : CharacterType(gui), rage(100) {}
 
     double takeDamage(WarriorGoblin*, double damage) {
-        double damageTotal = damage * 1;
+        double damageTotal = armor->takeDamage(damage * 1);
         CharacterType::takeDamage(damageTotal);
         return damageTotal;
     }
 
     double takeDamage(ArcherGoblin*, double damage) {
-        double damageTotal = damage * 1.5;
+        double damageTotal = armor->takeDamage(damage * 1.5);
         CharacterType::takeDamage(damageTotal);
         return damageTotal;
     }
 
     double takeDamage(MageGoblin*, double damage) {
-        double damageTotal = damage * 2;
+        double damageTotal = armor->takeDamage(damage * 2);
         CharacterType::takeDamage(damageTotal);
         return damageTotal;
     }
