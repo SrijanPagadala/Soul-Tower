751c4128eb6e751ea5e2bcc479529eb8e35a8c55
nabil-k <duomaster01@gmail.com>
2021-05-30 16:17:21 -0700

    Added warmogs armor

--- header/Armor.hpp
+++ header/Armor.hpp
@@ -1,11 +1,34 @@
 #pragma once
-
+#include "cMain.h"
 #include <string>
 
 class Armor {
 protected:
     double defenseValue;
-    std::string name;
     int tier;
+    cMain* gui;
 
+public:
+    Armor() {
+        gui = nullptr;
+        defenseValue = 0.9;
+        tier = 1;
+    }
+    Armor(cMain* gui) {
+        this->gui = gui;
+        defenseValue = 0.9;
+        tier = 1;
+    }
+    
+    double takeDamage(double damage) {
+        double reducedDamage = damage * (defenseValue - (tier * -0.1));
+        return reducedDamage;
+    }
+
+    void upgrade() {
+        tier += 1;
+    }
+
+    virtual void update(cMain* gui) = 0;
+    
 };
