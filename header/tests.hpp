bfb0918f6d8c027d4a2dacbaf0a8a8204b0d63d8
SiddharthSondhi <sondhisiddharth@gmail.com>
2021-05-31 19:21:33 -0700

    Added tests for wand, bow, and sword.

--- header/tests.hpp
+++ header/tests.hpp
@@ -15,6 +15,7 @@
 
 using namespace std;
 
+//////////////////////SWORD//////////////////////
 
 void weaponInitSword() {
 	Sword sword;
@@ -26,6 +27,53 @@ void weaponInitSword() {
 	}
 }
 
+
+void swordAttackWarriorGoblin() {
+	Enemy* enemy = new WarriorGoblin("enemy");
+	Sword weapon;
+
+	if (weapon.attack(enemy) == 20) {
+		cout << "SwordAttackWarriorGoblin [PASSED]" << endl;
+	}
+
+	else {
+		cout << "SwordAttackWarriorGoblin [FAILED]" << endl;
+	}
+}
+
+void swordAttackMageGoblin() {
+	Enemy* enemy = new MageGoblin("enemy");
+	Sword weapon;
+
+
+	if (weapon.attack(enemy) == 40) {
+		cout << "SwordAttackMageGoblin [PASSED]" << endl;
+	}
+
+	else {
+		cout << "SwordAttackMageGoblin [FAILED]" << endl;
+	}
+}
+
+void swordAttackArcherGoblin() {
+	Enemy* enemy = new ArcherGoblin("enemy");
+	Sword weapon;
+
+	if (weapon.attack(enemy) == 30) {
+		cout << "SwordAttackArcherGoblin [PASSED]" << endl;
+	}
+
+	else {
+		cout << "SwordAttackArcherGoblin [FAILED]" << endl;
+	}
+}
+
+//////////////////////SWORD//////////////////////
+
+
+///////////////////////BOW///////////////////////
+
+
 void weaponInitBow() {
 	Bow weapon;
 	if (weapon.getAttackDamage() == 20, weapon.getTier() == 1) {
@@ -35,6 +83,50 @@ void weaponInitBow() {
 		cout << "WeaponInitSword [FAILED]" << endl;
 	}
 }
+
+void bowAttackWarriorGoblin() {
+	Enemy* enemy = new WarriorGoblin("enemy");
+	Bow weapon;
+
+	if (weapon.attack(enemy) == 16) {
+		cout << "BowAttackWarriorGoblin [PASSED]" << endl;
+	}
+
+	else {
+		cout << "BowAttackWarriorGoblin [FAILED]" << endl;
+	}
+}
+
+void bowAttackMageGoblin() {
+	Enemy* enemy = new MageGoblin("enemy");
+	Bow weapon;
+
+
+	if (weapon.attack(enemy) == 25) {
+		cout << "BowAttackMageGoblin [PASSED]" << endl;
+	}
+
+	else {
+		cout << "BowAttackMageGoblin [FAILED]" << endl;
+	}
+}
+
+void bowAttackArcherGoblin() {
+	Enemy* enemy = new ArcherGoblin("enemy");
+	Bow weapon;
+
+	if (weapon.attack(enemy) == 20) {
+		cout << "BowAttackArcherGoblin [PASSED]" << endl;
+	}
+
+	else {
+		cout << "BowAttackArcherGoblin [FAILED]" << endl;
+	}
+}
+
+///////////////////////BOW///////////////////////
+
+//////////////////////WAND///////////////////////
 
 void weaponInitWand() {
 	Wand weapon;
@@ -46,12 +138,47 @@ void weaponInitWand() {
 	}
 }
 
+void wandAttackWarriorGoblin() {
+	Enemy* enemy = new WarriorGoblin("enemy");
+	Wand weapon;
 
-void swordAttack() {
+	if (weapon.attack(enemy) == 40) {
+		cout << "WandAttackWarriorGoblin [PASSED]" << endl;
+	}
 
-
-
+	else {
+		cout << "WandAttackWarriorGoblin [FAILED]" << endl;
+	}
 }
+
+void wandAttackMageGoblin() {
+	Enemy* enemy = new MageGoblin("enemy");
+	Wand weapon;
+
+	if (weapon.attack(enemy) == 20) {
+		cout << "WandAttackMageGoblin [PASSED]" << endl;
+	}
+
+	else {
+		cout << "WandAttackMageGoblin [FAILED]" << endl;
+	}
+}
+
+void wandAttackArcherGoblin() {
+	Enemy* enemy = new ArcherGoblin("enemy");
+	Wand weapon;
+
+	if (weapon.attack(enemy) == 25) {
+		cout << "WandAttackArcherGoblin [PASSED]" << endl;
+	}
+
+	else {
+		cout << "WandAttackArcherGoblin [FAILED]" << endl;
+	}
+}
+
+
+//////////////////////WAND///////////////////////
 
 
 
@@ -72,12 +199,24 @@ void runTests() {
 	cout << "--------------MAGE TESTS--------------" << endl << endl;
 
 	cout << "--------------SWORD TESTS--------------" << endl;
+	weaponInitSword();
+	swordAttackWarriorGoblin();
+	swordAttackMageGoblin();
+	swordAttackArcherGoblin();
 	cout << "--------------SWORD TESTS--------------" << endl << endl;
 
 	cout << "--------------WAND TESTS--------------" << endl;
+	weaponInitWand();
+	wandAttackWarriorGoblin();
+	wandAttackMageGoblin();
+	wandAttackArcherGoblin();
 	cout << "--------------WAND TESTS--------------" << endl << endl;
 
 	cout << "--------------BOW TESTS--------------" << endl;
+	weaponInitBow();
+	bowAttackWarriorGoblin();
+	bowAttackMageGoblin();
+	bowAttackArcherGoblin();
 	cout << "--------------BOW TESTS--------------" << endl << endl;
 
 	cout << "--------------MAGE FACTORY TESTS--------------" << endl;
