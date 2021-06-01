a8a2938e26961130750ee4a6944bbea919eab9ab
SrijanPagadala <srijanrpagadala@gmail.com>
2021-05-31 20:01:16 -0700

    Moved wxwidgets image handler

--- src/cApp.cpp
+++ src/cApp.cpp
@@ -15,10 +15,10 @@ cApp::~cApp() {
 }
 
 bool cApp::OnInit() {
+	wxInitAllImageHandlers();
 	if (tests)
 		runTests();
 
-	wxInitAllImageHandlers();
 	m_frame_1 = new cMain();
 	m_frame_1->Show();
 	SetTopWindow(m_frame_1);
@@ -28,7 +28,7 @@ bool cApp::OnInit() {
 }
 
 BEGIN_EVENT_TABLE(cMain, wxFrame)
-EVT_MENU(Minimal_Quit, cMain::OnQuit)
-EVT_MENU(THREAD_STOP, cMain::OnStopThread)
-EVT_BUTTON(BUTTON_Choice, cMain::SubmitChoice) // Tell the OS to run MainFrame::OnExit when
+	EVT_MENU(Minimal_Quit, cMain::OnQuit)
+	EVT_MENU(THREAD_STOP, cMain::OnStopThread)
+	EVT_BUTTON(BUTTON_Choice, cMain::SubmitChoice) // Tell the OS to run MainFrame::OnExit when
 END_EVENT_TABLE() // The button is pressed
